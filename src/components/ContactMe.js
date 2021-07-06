cd1d6d7bf728a284be3ce5b7e2bae63925122b9f
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2021-07-06 11:38:43 -0400

    home contact-me complete

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,7 +1,16 @@
 import "./ContactMe.scss";
+import Button from "react-bootstrap/Button";
 
 const ContactMe = () => {
-  return <div>ContactMe</div>;
+  return (
+    <div className="contact-me my-5 py-5 d-flex align-items-center">
+      <h2>Interested in doing a project together?</h2>
+      <div className="line-contact px-4 mx-4">
+        <hr></hr>
+      </div>
+      <Button variant="outline-secondary">Contact Me</Button>
+    </div>
+  );
 };
 
 export default ContactMe;
